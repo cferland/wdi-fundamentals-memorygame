860ca6a2c6c5e27dc2ddbcf99e6d3c3a25ee3bfc
Corey Ferland <ferlandc@gmail.com>
2019-10-20 12:32:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,20 +22,44 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
+var matches = 0;
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		matches++;
+		//check if all matches found
+		if (matches === 2) {
+			alert("Another match! Congratulations, you won!");
+		} else {
+			alert("You found a match!");
+		}
 	} else {
 		alert("Sorry, try again.");
+		//reset game board
+		document.getElementById('game-board').innerHTML = "";
+		createBoard();
 	}
+	//empty array for next match
+	cardsInPlay = [];
 }
-function flipCard(cardID) {
+function flipCard() {
+	var cardID = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardID].rank + " of " + cards[cardID].suit);
 	console.log(cards[cardID].cardImage);
+	this.setAttribute('src', cards[cardID].cardImage);
+	//prevent clicking flipped cards
+	this.removeEventListener('click', flipCard);
 	cardsInPlay.push(cards[cardID].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
